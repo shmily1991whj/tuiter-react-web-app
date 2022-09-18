af471abef8c99a36d1dcb5f502776176f772d525
corrine <corrine@corrines-mbp.lan>
2022-09-18 10:24:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+            welcome to web development fall
+        </h1>
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
         <a
           className="App-link"
           href="https://reactjs.org"
